da74caf47eaeb43c4c38f9c63efbd786b6abd076
Роман Соколов <sokoloff.rv@gmail.com>
2023-01-29 17:54:18 +0600

    Убраны комментарии в коде класса ConverterCSV

--- src/Utils/ConverterCSV.php
+++ src/Utils/ConverterCSV.php
@@ -13,73 +13,58 @@ class ConverterCSV
     private $result = [];
     private $error = null;
 
-    /* конструктор, присваивает значение переменным */
     public function __construct(string $fileName, array $columns)
     {
         $this->fileName = $fileName;
         $this->columns = $columns;
     }
 
-    /* выполняет запись данных из CSV файла в массив */
     public function import(): void
     {
-        /* выводит сообщение об ошибке, если заданы неверные заголовки столбцов */
         if (!$this->validateColumns($this->columns)) {
             throw new ExceptionFileFormat("Заданы неверные заголовки столбцов!");
         }
 
-        /* выводит сообщение об ошибке, если заданный файл не существует */
         if (!file_exists($this->fileName)) {
             throw new ExceptionSourceFile("Выбранный файл не существует!");
         }
 
-        /* создает объект файла */
         try {
             $this->fileObject = new SplFileObject($this->fileName);
         }
-        /* выводит сообщение об ошибке, если не удалось прочитать файл */ catch (RuntimeException $exception) {
+        catch (RuntimeException $exception) {
             throw new ExceptionSourceFile("Не удалось открыть файл на чтение!");
         }
 
-        /* запись заголовков файла в переменную */
         $header_data = $this->getHeaderData();
 
-        /* проверка наличия в файле переданных столбцов */
         if ($header_data !== $this->columns) {
             throw new ExceptionFileFormat("Исходный файл не содержит необходимых столбцов!");
         }
 
-        /* построчная запись данных из файла в массив */
         foreach ($this->getNextLine() as $line) {
             $this->result[] = $line;
         }
     }
 
-    /* выводит массив с результатами (если уже был выполнен импорт) */
     public function getData(): array
     {
         return $this->result;
     }
 
-    /* получение заголовков файла */
     private function getHeaderData(): ?array
     {
-
-        /* перемотка в начало файла */
         $this->fileObject->rewind();
 
-        /* получение строки файла и её интерпретация как CSV */
         $data = $this->fileObject->fgetcsv();
 
         return $data;
     }
 
-    /* получение следующей строки */
     private function getNextLine(): ?iterable
     {
         $result = null;
 
-        /* получение следующей строки пока файл не закончится */
         while (!$this->fileObject->eof()) {
             yield $this->fileObject->fgetcsv();
         }
@@ -87,15 +72,11 @@ class ConverterCSV
         return $result;
     }
 
-    /* проверяет корректность переданных заголовков столбцов */
     private function validateColumns(array $columns): bool
     {
         $result = true;
 
-        /* проверяем есть ли заголовоки вообще */
         if (count($columns)) {
-
-            /* проверяем являются ли они строками */
             foreach ($columns as $column) {
                 if (!is_string($column)) {
                     $result = false;
@@ -108,7 +89,6 @@ class ConverterCSV
         return $result;
     }
 
-    /* генерирует файл SQL */
     public function generateSqlFile(string $tableName): void
     {
         $this->tableName = $tableName;
