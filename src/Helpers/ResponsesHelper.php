f38556fe2a21fd0eb30ee19de7f0ca768c21169b
Роман Соколов <sokoloff.rv@gmail.com>
2023-03-02 23:06:16 +0600

    Оптимизировал код хелпера и добавил пояснения

--- src/Helpers/ResponsesHelper.php
+++ src/Helpers/ResponsesHelper.php
@@ -5,6 +5,13 @@ use Taskforce\Models\Response;
 
 class ResponsesHelper
 {
+    /**
+     * Проверяет, может ли пользователь просматривать список откликов на задание.
+     * @param array $responses массив откликов на задание
+     * @param int $user_id идентификатор пользователя
+     * @param int $customer_id идентификатор заказчика задания
+     * @return bool true, если пользователь может просмотреть список, false - в противном случае.
+     */
     public static function userCanSeeResponsesList(array $responses, int $user_id, int $customer_id): bool {
         if ($responses) {
             if ($user_id === $customer_id) {
@@ -19,10 +26,14 @@ class ResponsesHelper
         return false;
     }
 
+    /**
+     * Проверяет, может ли пользователь просматривать отклик на задание.
+     * @param int $user_id идентификатор пользователя
+     * @param int $customer_id идентификатор заказчика задания
+     * @param int $executor_id идентификатор исполнителя, оставившего отклик на задание
+     * @return bool true, если пользователь может просмотреть отклик, false - в противном случае.
+     */
     public static function userCanSeeResponse(int $user_id, int $customer_id, int $executor_id): bool {
-        if ($user_id === $customer_id || $user_id === $executor_id) {
-            return true;
-        }
-        return false;
+        return $user_id === $customer_id || $user_id === $executor_id;
     }
 }
