3d3aa501ab967b52fec148aed8190f59d8ee4d89
Роман Соколов <sokoloff.rv@gmail.com>
2023-03-02 23:12:08 +0600

    Улучшил читаемость хелпера

--- src/Helpers/RateHelper.php
+++ src/Helpers/RateHelper.php
@@ -1,18 +1,27 @@
 <?php
+
 namespace Taskforce\Helpers;
 
 class RateHelper
 {
-    public static function getStars($rate): string
+    /**
+     * Возвращает HTML-код со звездами, отображающими рейтинг.
+     * @param float $rate значение рейтинга.
+     * @return string HTML-код со звездами.
+     */
+    public static function getStars(float $rate): string
     {
         $count = round($rate);
         $stars = '';
-        for($i = 0; $i < $count; $i++) {
+
+        for ($i = 0; $i < $count; $i++) {
             $stars .= "<span class=\"fill-star\">&nbsp;</span>";
         }
-        for($i = 0; $i < 5 - $count; $i++) {
+
+        for ($i = 0; $i < 5 - $count; $i++) {
             $stars .= "<span>&nbsp;</span>";
         }
+
         return $stars;
     }
 }
