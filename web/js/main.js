21e6af39bab7b95058f9819ff58a12dbc82d96d3
Роман Соколов <sokoloff.rv@gmail.com>
2023-03-04 13:25:51 +0600

    Добавлен потерянный код выставления оценки

--- web/js/main.js
+++ web/js/main.js
@@ -46,3 +46,28 @@ if (buttonInput) {
         }
     });
 }
+
+let starRating = document.querySelector(".active-stars");
+
+if (starRating) {
+    starRating.addEventListener("click", function (event) {
+        let stars = event.currentTarget.childNodes;
+        let rating = 0;
+
+        for (let i = 0; i < stars.length; i++) {
+            let element = stars[i];
+
+            if (element.nodeName === "SPAN") {
+                element.className = "fill-star";
+                rating++;
+            }
+
+            if (element === event.target) {
+                break;
+            }
+        }
+
+        let inputField = document.getElementById('acceptance-form-rate');
+        inputField.value = rating;
+    });
+}
