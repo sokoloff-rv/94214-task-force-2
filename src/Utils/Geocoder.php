072f1cc1a5a805ae37b12f89baf21a938548e951
Роман Соколов <sokoloff.rv@gmail.com>
2023-03-09 17:28:01 +0600

    Добавлена утилита геокодера

---
+++ src/Utils/Geocoder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Taskforce\Utils;
+
+use GuzzleHttp\Client;
+
+class Geocoder
+{
+    /**
+     * Определяет координаты по заданному адресу, используя сервисы картографии.
+     *
+     * @param string $location Адрес для определения координат.
+     * @return array Массив, содержащий координаты в формате [долгота, широта].
+     */
+    public static function determineCoordinates(string $location): array
+    {
+        $apiKey = 'e666f398-c983-4bde-8f14-e3fec900592a';
+
+        $client = new Client([
+            'base_uri' => 'https://geocode-maps.yandex.ru/',
+        ]);
+
+        $response = $client->request('GET', '1.x', [
+            'query' => ['geocode' => $location, 'apikey' => $apiKey, 'format' => 'json']
+        ]);
+
+        $content = $response->getBody()->getContents();
+        $responseData = json_decode($content, true);
+
+        $coordinates = $responseData['response']['GeoObjectCollection']['featureMember']['0']['GeoObject']['Point']['pos'];
+
+        return explode(' ', $coordinates);
+    }
+}
