3edcefd69ae7bd18bc6c82c3726dd28647518bcc
Роман Соколов <sokoloff.rv@gmail.com>
2023-03-16 23:25:14 +0600

    Перенес редирект из модели в контроллер

--- controllers/RegistrationController.php
+++ controllers/RegistrationController.php
@@ -14,6 +14,7 @@ class RegistrationController extends NotSecuredController
         if (Yii::$app->request->getIsPost()) {
             $registration->load(Yii::$app->request->post());
             $registration->createUser();
+            Yii::$app->response->redirect(['tasks']);
         }
 
         return $this->render('index', ['registration' => $registration]);
