9b4ec7cba6ba3cf21d9f4a7a85c5c625bfe38ff3
Роман Соколов <sokoloff.rv@gmail.com>
2023-03-21 19:41:51 +0600

    Добавлена загрузка даты рождения из ВК

--- models/VkUser.php
+++ models/VkUser.php
@@ -14,6 +14,8 @@ class VkUser extends Model
         $user = new User;
         $user->name = $userData['first_name'] . ' ' . $userData['last_name'];
         $user->email = $userData['email'];
+        $birthayDate = \DateTime::createFromFormat('d.m.Y', $userData['bdate']);
+        $user->birthday = $birthayDate ? $birthayDate->format('Y-m-d') : null;
         $user->password = Yii::$app->getSecurity()->generatePasswordHash('password');
         $user->city_id = City::getIdByName($userData['city']['title']);
         $user->vk_id = $userData['user_id'];
