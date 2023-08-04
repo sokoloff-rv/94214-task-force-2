7ed92543d3c7dec1cb4c240c86cceecb1368e7b3
Роман Соколов <sokoloff.rv@gmail.com>
2023-08-04 17:54:22 +0600

    Добавлен favicon

--- views/layouts/landing.php
+++ views/layouts/landing.php
@@ -27,6 +27,13 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title><?=Html::encode($this->title) . ' - Taskforce'?></title>
     <?php $this->head()?>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1e90ff">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="theme-color" content="#ffffff">
 </head>
 <body class="landing">
 <?php $this->beginBody()?>
