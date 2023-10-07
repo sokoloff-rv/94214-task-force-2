13d62b3a93695d735c91dbf9b3a896a2a80ef669
sokoloff-rv <sokoloff.rv@gmail.com>
2023-10-07 16:39:35 +0300

    Удален файл с конфигурацией БД из репозитория

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=sokolo9w_tskfrce',
-    'username' => 'sokolo9w_tskfrce',
-    'password' => 'K%egZu7f',
-    'charset' => 'utf8',
-];
